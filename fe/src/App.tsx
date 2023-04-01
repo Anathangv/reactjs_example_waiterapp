5a662a166a82817d1a2a7a7c4f0208244cb0f4c4
Anathan <anathan.gv@gmail.com>
2023-04-01 19:00:37 -0300

    feat: remove comments

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,12 +3,6 @@ import { Header } from './components/Header/';
 import { Orders } from './components/Orders';
 import {GlobalStyles} from './styles/GlobalStyles';
 
-/*
-  reduce
-  Intl.NumberFormat
-*/
-
-
 export function App(){
   return(
     <React.Fragment>
